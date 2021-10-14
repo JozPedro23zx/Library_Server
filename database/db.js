7419fea468d04ac0d4e0faeb1af0b4544e048664
JozPedro23zx <josepedro23zx@gmail.com>
2021-10-14 20:01:39 -0300

    ssl config

--- database/db.js
+++ database/db.js
@@ -2,6 +2,11 @@ const dotenv = require('dotenv')
 const Sequelize = require('sequelize');
  
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: true
+    }
+});
 
 module.exports = sequelize;
