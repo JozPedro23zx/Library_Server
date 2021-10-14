9e4227245201cf210f85eb531b5ee4be14851297
JozPedro23zx <josepedro23zx@gmail.com>
2021-10-14 19:56:53 -0300

    remove config dotenv

--- database/db.js
+++ database/db.js
@@ -1,9 +1,5 @@
 const dotenv = require('dotenv')
 const Sequelize = require('sequelize');
-
-const result = dotenv.config({path: '../.env'})
-
-if(result.error) console.log(result.error)
  
 
 const sequelize = new Sequelize(process.env.DATABASE_URL);
